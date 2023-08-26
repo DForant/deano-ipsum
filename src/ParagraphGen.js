4b2517485386e70ee5a6eb2a0675cfca20483028
deano252 <dforant@gmail.com>
2023-08-26 07:04:17 -0400

    initial comment of TODO

--- src/ParagraphGen.js
+++ src/ParagraphGen.js
@@ -3,6 +3,23 @@ export const buildParagraph = (allowParagraphs, size, currentIpsum) => {
   let sentences = 3
   let i=0
 
+  /*
+  TODO
+
+  Refactor the size of the paragraph to be a fixed length rather then number
+  of sentences Use the following as a guide
+
+  small - 200 characters
+  Med - 500 characters
+  Large - 800 characters
+  xtra-large - 1100 characters
+
+  Also try to put a for loop to randomly pull another sentence
+  if the length is under the specified amount. try 10 times
+  and after 10, just move on.
+
+  */
+
   switch(size){
     case 'small':
       sentences = 3
@@ -24,6 +41,7 @@ export const buildParagraph = (allowParagraphs, size, currentIpsum) => {
     }
     paragraph = paragraph.substring(0, paragraph.length-1)
     paragraph = allowParagraphs?'<p>'+paragraph+'</p>':paragraph
+    paragraph = paragraph + `--- ${paragraph.length} ---`
 
     return paragraph
 }
